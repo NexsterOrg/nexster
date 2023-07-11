1c24d3ec41651491e880b37b32b3f0e738ee4cee
NamalSanjaya <namalsanjaya23@gmail.com>
2023-07-11 13:11:18 +0530

    Add reacion repo methods

---
+++ timeline/pkg/repos/reaction/interface.go
@@ -0,0 +1,18 @@
+package reaction
+
+import "context"
+
+type Interface interface {
+	UpdateReactions(ctx context.Context, fromUserId, toMediaId, key string, updateDoc map[string]interface{}) error
+}
+
+type Reaction struct {
+	From       string `json:"_from,omitempty"`
+	To         string `json:"_to,omitempty"`
+	Key        string `json:"_key,omitempty"`
+	Like       bool   `json:"like"`
+	Love       bool   `json:"love"`
+	Laugh      bool   `json:"laugh"`
+	Sad        bool   `json:"sad"`
+	Insightful bool   `json:"insightful"`
+}
