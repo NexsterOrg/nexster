be544a66118690b3d5c304288d0c61266ee5d4f8
NamalSanjaya <namalsanjaya23@gmail.com>
2023-07-15 00:01:10 +0530

    Add method to list media with owner info

--- pkgs/models/media/interface.go
+++ pkgs/models/media/interface.go
@@ -9,12 +9,19 @@ const MediaColl string = "media" // Name of collection
 type Interface interface {
 	MkMediaDocId(key string) string
 	ListMedia(ctx context.Context, query string, bindVars map[string]interface{}) ([]*Media, error)
+	ListMediaWithOwner(ctx context.Context, query string, bindVars map[string]interface{}) ([]*MediaWithOwner, error)
 }
 
 type Media struct {
+	Key         string `json:"_key"`
 	Link        string `json:"link"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	CreateDate  string `json:"created_date"`
 	Size        int    `json:"size"`
+}
+
+type MediaWithOwner struct {
+	Media   Media  `json:"media"`
+	OwnerId string `json:"owner_id"`
 }
