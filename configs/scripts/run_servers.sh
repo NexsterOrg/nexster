36c0e5ff67af34ed218b796b0b9850a0fa946545
NamalSanjaya <namalsanjaya23@gmail.com>
2023-12-31 12:43:28 +0530

    Add server setup script

---
+++ configs/scripts/run_servers.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+PROJECT_ROOT_DIR=/home/namal/workspace/my_work/projects/nexster/dev_work/nexster-runtime/nexster
+LOGS_DIR=/home/namal/workspace/my_work/projects/nexster/dev_work/nexster-runtime/nexster/logs
+
+cd $PROJECT_ROOT_DIR
+
+if [ ! -d "$PROJECT_ROOT_DIR/logs" ]; then
+    mkdir logs
+    echo "logs directory created."
+else
+    echo "logs directory already exists."
+fi
+
+# Run content server
+cd content/cmd
+nohup go run main.go > $LOGS_DIR/content_server.log 2>&1 &
+
+# Run search server
+cd ../../search/cmd
+nohup go run main.go > $LOGS_DIR/search_server.log 2>&1 &
+
+# Run space server
+cd ../../space/cmd
+nohup go run main.go > $LOGS_DIR/space_server.log 2>&1 &
+
+# Run timeline server
+cd ../../timeline/cmd
+nohup go run main.go > $LOGS_DIR/timeline_server.log 2>&1 &
+
+# Run usrmgmt server
+cd ../../usrmgmt/cmd
+nohup go run main.go > $LOGS_DIR/usrmgmt_server.log 2>&1 &
